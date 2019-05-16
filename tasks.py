5cdf00c739057116522dc1c3ca329d5497254347
chrisguy <chris@chrisguy.co>
2019-05-16 13:19:45 +0100

    Added class to task creation

--- tasks.py
+++ tasks.py
@@ -1,19 +1,51 @@
 """Tasks."""
+import datetime
+
+import utilities
+
+fmt = '%d/%m/%Y'
 
 
 class Task(list):
     """Define the Task class."""
 
-    name = None
-    date = None
-    time = None
-    note = None
-
-
-class Add_New(Task):
-    """Define the Add_New class."""
-
     def __init__(self):
         """Customize the class init."""
-        self.name = input("What is the task? ")
-        self.data = input("What is the date? ")
+        self.date = None
+        self.name = None
+        self.time = None
+        self.note = None
+
+    def add_date(self):
+        """Set the task date."""
+        utilities.show_add_task_title()
+        while True:
+            # Get the date of the task from the user
+            task_date = input("\nWhat is the date of the task? \n"
+                              "Please use 'DD/MM/YYYY format: ")
+            try:
+                task_date = datetime.datetime.strptime(task_date, fmt)
+            except ValueError:
+                utilities.show_add_task_title()
+                print("Sorry '{}' is not in the correct date format. "
+                      "Please try again.\n".format(task_date))
+                continue
+            else:
+                self.date = task_date  # Set the date
+                self.get_task_name()
+                break
+
+    def get_task_name(self):
+        """Set the task name."""
+        utilities.show_add_task_title()
+        while True:
+            task_name = str(input('\nWhat is the name of the task? '))
+            if task_name == '':
+                print("sorry, task name cannot be blank.")
+                continue
+            else:
+                self.name = task_name
+                print('Date = {}'.format(self.date))
+                print('Name = {}'.format(self.name))
+                break
+
--- utilities.py
+++ utilities.py
@@ -5,3 +5,19 @@ import os
 def clear_screen():
     """Clear the console screen."""
     os.system("cls" if os.name == 'nt' else "clear")
+
+
+def show_main_menu_options():
+    """Print the main menu options."""
+    clear_screen()
+    print('\nWork Log : Main Menu'.upper()+'\n'+'-'*20)
+    print('What would you like to do?')
+    print('a) Add a new entry')
+    print('b) Serach the existing entries')
+    print('c) Quit')
+
+
+def show_add_task_title():
+    """Print the main menu options."""
+    clear_screen()
+    print('\nWork Log : Add a task'.upper()+'\n'+'-'*24)

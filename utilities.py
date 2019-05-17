ee055e048751f45880b87b248f5b7004b9ac1c2e
chrisguy <chris@chrisguy.co>
2019-05-17 18:30:23 +0100

    1st stage of search added

--- utilities.py
+++ utilities.py
@@ -11,7 +11,7 @@ def show_main_menu_options():
     """Print the main menu options."""
     clear_screen()
     print('\nWork Log : Main Menu'.upper()+'\n'+'-'*20)
-    print('What would you like to do?')
+    print('What would you like to do?\n')
     print('a) Add a new entry')
     print('b) Serach the existing entries')
     print('c) Quit')
@@ -21,3 +21,17 @@ def show_add_task_title():
     """Print the main menu options."""
     clear_screen()
     print('\nWork Log : Add a task'.upper()+'\n'+'-'*22)
+
+
+def show_search_menu_options():
+    """Print the main menu options."""
+    clear_screen()
+    print('\nWork Log : Search Menu'.upper()+'\n'+'-'*20)
+    print('What do you want to search by:\n')
+    print('a) Exact date')
+    print('b) Range of dates')
+    print('c) Time spent')
+    print('d) Exact search')
+    print('e) RegEx pattern')
+    print('f) [Return to main menu]')
+
--- work_log.py
+++ work_log.py
@@ -1,8 +1,12 @@
 """Work log main file."""
 
-import utilities
+import csv
+import datetime
 
+import utilities
 from tasks import Task
+
+fmt = '%d/%m/%Y'
 
 
 def main_menu():
@@ -15,7 +19,8 @@ def main_menu():
             add_new_task()
             break
         if selction.upper() == 'B':
-            print('b')
+            go_search()
+            break
         if selction.upper() == 'C':
             utilities.clear_screen()
             print('\nThanks for using WORK LOG!\n')
@@ -30,6 +35,45 @@ def add_new_task():
     """Create a class instance and populate it."""
     t = Task()
     t.add_date()
+
+
+def go_search():
+    """Show the serach menu."""
+    utilities.show_search_menu_options()
+
+    while True:
+        selction = input("\nEnter 'a', 'b', 'c', 'd', 'e' or 'f' > ")
+
+        if selction.upper() == 'A':
+            serach_date()
+            break
+        if selction.upper() == 'B':
+            print('B')
+            break
+        if selction.upper() == 'C':
+            print('C')
+            break
+        if selction.upper() == 'D':
+            print('D')
+            break
+        if selction.upper() == 'E':
+            print('E')
+            break
+        if selction.upper() == 'F':
+            main_menu()
+            break
+
+
+def serach_date():
+    """Serach by exact date."""
+    # search_input = datetime.datetime.strptime('1969-05-31 00:00:00', fmt)
+
+    with open('tasks.csv', newline='') as csvfile:
+        taskreader = csv.DictReader(csvfile, delimiter=',')
+        rows = list(taskreader)
+        for row in rows[0:len(rows)]:
+            if row['task_date'] == '1969-05-31 00:00:00':
+                print(row)
 
 
 if __name__ == "__main__":
